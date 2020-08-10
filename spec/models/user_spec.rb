8db085faedc3db1922fb3324584fc7cffcebf320
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-08-10 21:40:55 +0900

    change to subject

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,43 +1,43 @@
 describe User do
   let(:password) { "Password01" }
-  let(:user) { create :user, password: password }
+  subject { create :user, password: password }
 
   describe "#valid_password?" do
     it "returns true if correct" do
-      expect(user).to be_valid_password password
+      expect(subject).to be_valid_password password
     end
 
     it "returns false if incorrect" do
-      expect(user).not_to be_valid_password "123"
+      expect(subject).not_to be_valid_password "123"
     end
   end
 
   describe "#password" do
     it "is valid if at least 8-characters long" do
-      user = build :user
-      user.password = user.confirmation_password = "1234567"
-      expect(user).not_to be_valid
+      subject = build :user
+      subject.password = subject.confirmation_password = "1234567"
+      expect(subject).not_to be_valid
 
-      user.password = user.confirmation_password = "12345678"
-      expect(user).to be_valid
+      subject.password = subject.confirmation_password = "12345678"
+      expect(subject).to be_valid
 
-      user.password = user.confirmation_password = "1" * 10
-      expect(user).to be_valid
+      subject.password = subject.confirmation_password = "1" * 10
+      expect(subject).to be_valid
     end
   end
 
   describe "#email" do
     it "is lowercased when saved" do
-      expect(user).to be_valid
+      expect(subject).to be_valid
 
-      user.email = "ADAMNOTO@EXAMPLE.org"
-      expect(user.save).to be_truthy
-      expect(user.email).to eq "adamnoto@example.org"
+      subject.email = "ADAMNOTO@EXAMPLE.org"
+      expect(subject.save).to be_truthy
+      expect(subject.email).to eq "adamnoto@example.org"
     end
 
     it "is unique" do
       expect {
-        create(:user, email: user.email.upcase)
+        create(:user, email: subject.email.upcase)
       }.to raise_error ActiveRecord::RecordNotUnique
     end
 
@@ -52,16 +52,16 @@ describe User do
   describe "#username" do
     context "when new record" do
       it "equals to local part of the email" do
-        user = build :user
-        expect(user).to be_new_record
+        subject = build :user
+        expect(subject).to be_new_record
 
         local_part = "adam12345_cool"
         email = "#{local_part}@example.org"
-        user.email = email
+        subject.email = email
 
-        user.save
+        subject.save
 
-        expect(user.username).to eq local_part
+        expect(subject.username).to eq local_part
       end
 
       it "tolerates username less than 5 characters long" do
@@ -77,11 +77,11 @@ describe User do
 
     context "when not new record" do
       it "enforces length requirement" do
-        user = create :user
-        user.username = "abcd"
-        expect(user).not_to be_valid
-        user.username = "abcde"
-        expect(user).to be_valid
+        subject = create :user
+        subject.username = "abcd"
+        expect(subject).not_to be_valid
+        subject.username = "abcde"
+        expect(subject).to be_valid
       end
     end
   end
