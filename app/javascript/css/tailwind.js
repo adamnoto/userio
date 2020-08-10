618fa31bfb6d747a960c02dc12d0cd6d269fde54
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-08-10 14:31:39 +0900

    setup react & friends for some parts of front-end

---
+++ app/javascript/css/tailwind.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          default: "#3d68ff",
+          "600": "#224ff2",
+        },
+      },
+      spacing: {
+        "14": "3.5rem",
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem",
+        "168": "42rem",
+        "192": "48rem",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
