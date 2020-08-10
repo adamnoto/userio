d91aa7ed9936c65257ceed3dd6b7724f048140c3
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-08-10 14:16:36 +0900

    add spec for email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,28 @@ describe User do
       expect(user).not_to be_valid_password "123"
     end
   end
+
+  describe "#email" do
+    it "is lowercased when saved" do
+      expect(user).to be_valid
+
+      user.email = "ADAMNOTO@EXAMPLE.org"
+      expect(user.save).to be_truthy
+      expect(user.email).to eq "adamnoto@example.org"
+    end
+
+    it "is unique" do
+      expect {
+        create(:user, email: user.email.upcase)
+      }.to raise_error ActiveRecord::RecordNotUnique
+    end
+
+    it { is_expected.not_to allow_value("").for(:email) }
+    it { is_expected.not_to allow_value("foo.bar").for(:email) }
+    it { is_expected.not_to allow_value("foo.bar#example.org").for(:email) }
+    it { is_expected.to allow_value("f.o.o.b.a.r@example.org").for(:email) }
+    it { is_expected.to allow_value("foo+bar@example.org").for(:email) }
+    it { is_expected.to allow_value("foo.bar@sub.example.co.id").for(:email) }
+  end
+
 end
