b7c961a3d33a2b5c85aaab047e76ce3fcaf7adb4
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-08-10 21:57:20 +0900

    reset password

--- app/controllers/sessions/reset_passwords_controller.rb
+++ app/controllers/sessions/reset_passwords_controller.rb
@@ -1,9 +1,34 @@
 class Sessions::ResetPasswordsController < Sessions::BaseController
   def show
-    token = PasswordResetToken.find_by_token params[:reset_password_id]
-
     if token.nil? || !token.active?
-      redirect_to sessions_reset_token_path, alert: "Cannot reset a password using this link. Please request a new link."
+      return redirect_to sessions_reset_token_path,
+        alert: "Cannot reset a password using this link. Please request a new link."
+    end
+
+    @user = token.user
+  end
+
+  def update
+    @user = token.user
+
+    if @user.update permitted_params
+      redirect_to sessions_sign_in_path, notice: "Now you may sign in with the new password"
+      # normally, we should invalidate the token, but it's not strictly on the requiremen
+      # so to keep the project much simpler, it's left out
+    else
+      flash[:alert] = @user.errors.full_messages.join(". ")
+      render "show"
     end
   end
+
+  private
+
+    def token
+      @token ||= PasswordResetToken.find_by_token params[:reset_password_id]
+    end
+
+    def permitted_params
+      params.require(:user).permit(:password)
+    end
+
 end
--- spec/features/reset_password_spec.rb
+++ spec/features/reset_password_spec.rb
@@ -18,4 +18,20 @@ describe "Reset password" do
     expect(ActionMailer::Base.deliveries).to be_blank
     expect(current_path).to eq sessions_sign_in_path
   end
+
+  scenario "Visiting using correct reset token & fill in the form correctly: password updated" do
+    token = create :password_reset_token
+    user = token.user
+
+    visit sessions_reset_password_path(token)
+    fill_in "Password", with: "Password02"
+    click_on "Reset my password"
+
+    expect(page).to have_content "Now you may sign in with the new password"
+  end
+
+  scenario "Visiting using invalid link: redirected" do
+    visit sessions_reset_password_path("INVALIDTOKEN")
+    expect(current_path).to eq sessions_reset_token_path
+  end
 end
