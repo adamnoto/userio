b63e801f00f33131b27661d09205de4b6d746032
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-08-10 13:39:48 +0900

    email format validation & normalization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP,
+    message: "must be a valid email address"
+
+  before_save :normalize_email, if: :email_changed?
+
+  private
+
+    def normalize_email
+      # note: ensuring adamnoto@hey.com and ADAMNOTO@hey.com are of the same account
+      write_attribute :email, email.downcase
+    end
+
 end
